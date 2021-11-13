52a0730a72b3bf955c83d1a3d64b3cb3d45990e1
Emir Güvenni <contact@emirguvenni.com>
2021-11-13 16:35:52 +0300

    Moved everything into a single file

--- src/interfaces/JobSenderOptions.ts
+++
@@ -1,8 +0,0 @@
-import { RedisClient } from 'redis';
-
-export default interface JobSenderOptions {
-  jobName: string;
-  jobData?: object;
-  jobQueueName: string;
-  redisConnection?: RedisClient;
-}
